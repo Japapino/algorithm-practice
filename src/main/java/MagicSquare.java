cd3554a445df6e12230f35f3419b226625e09908
Japapino <stefan.murakami@gmail.com>
2018-08-28 12:28:36 -0500

    passed row check test

---
+++ src/main/java/MagicSquare.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+
+public class MagicSquare {
+
+    //stores all possible 3x3 magic squares
+    int[][] array;
+
+    public MagicSquare(int[][] array){
+        this.array = array;
+    }
+
+    public int[][] getSquare(){
+        return array;
+    }
+
+    public boolean checkRows(){
+
+        int sum = 0;
+        ArrayList<Integer> sums = new ArrayList<>();
+
+        for (int i = 0; i < array.length; i++){
+            sum = 0;
+            for (int j = 0; j < array.length; j++){
+                sum += array[i][j];
+            }
+
+            sums.add(sum);
+        }
+
+        int check = sums.get(0);
+
+        for (int z = 0 ; z < sums.size(); z++){
+            if (sums.get(z) != check){
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+}
--- src/test/java/magicSquareCheckTest.java
+++ src/test/java/magicSquareCheckTest.java
@@ -1,4 +1,21 @@
+import org.junit.Test;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertTrue;
+
 public class magicSquareCheckTest {
+
+
+
+    @Test
+    public void checkIfSumOfAllRowsAreEqual(){
+        int[][] array = new int[][]{
+                {-1,1,1},
+                {-1,1,1},
+                {-1,1,1}};
+        MagicSquare test = new MagicSquare(array);
+        boolean check = test.checkRows();
+        assertTrue(check);
+    }
 
 
 }
